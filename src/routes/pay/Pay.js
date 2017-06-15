64c5aa98c48bec084172dfe745cd45d27375cfcd
zylee <zylee005@163.com>
2017-06-15 23:29:33 +0800

    icon

--- src/routes/pay/Pay.js
+++ src/routes/pay/Pay.js
@@ -4,10 +4,16 @@ import {
   WingBlank,
   WhiteSpace,
   Radio,
+  Icon,
   NavBar,
   TextareaItem
 } from 'antd-mobile';
+import style from './index.less';
 const RadioItem = Radio.RadioItem;
+const weixinIcon = require('!svg-sprite!../../svg/weixin.svg');
+const aliPayIcon = require('!svg-sprite!../../svg/alipay.svg');
+const yinlianIcon = require('!svg-sprite!../../svg/yinlian.svg');
+
 class Pay extends React.Component {
   constructor(props) {
     super(props);
@@ -22,16 +28,16 @@ class Pay extends React.Component {
     const data = [
       {
         value: 0,
-        label: '微信支付'
+        label: '微信支付',
+        icon: weixinIcon
       }, {
         value: 1,
-        label: '支付宝支付'
+        label: '支付宝支付',
+        icon: aliPayIcon
       }, {
         value: 2,
-        label: '银行卡支付'
-      }, {
-        value: 3,
-        label: '会员卡支付'
+        label:'银行卡支付',
+        icon: yinlianIcon
       }
     ];
     let {value} = this.state
@@ -44,7 +50,10 @@ class Pay extends React.Component {
               key={i.value}
               checked={value === i.value}
               onChange={() => this.onChange(i.value)}>
-              {i.label}
+              <div className={style['pay-item']}>
+                <Icon type={i.icon}/>
+                <span>{i.label}</span>
+              </div>
             </RadioItem>
           ))}
         </List>
